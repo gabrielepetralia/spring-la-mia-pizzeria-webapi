001cc8345b3c74ab2619d2444431e848209a9c64
gabrielepetralia <gabri.petralia03@gmail.com>
2023-10-17 17:07:39 +0200

    install Vue and dependencies

---
+++ frontend/vue-pizzeria-webapi/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import "@fortawesome/fontawesome-free/css/all.css"
+import "@fortawesome/fontawesome-free/js/all.js"
+
+export default {
+  name: "App",
+
+  components: {
+  
+  }
+}
+</script>
+
+<template>
+  <h1>Pizzeria</h1>
+  <i class="fa-solid fa-house"></i>
+</template>
+
+<style lang="scss">
+  @use "./scss/main.scss";
+</style>
